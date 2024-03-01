e8d0ce9c1794516a911c64e2a6c27cb8da50adb0
Justin Kerr <justin.g.kerr@gmail.com>
2024-03-01 02:52:52 -0800

    Refactor splatfacto gaussian params with a ParameterDict (#2946)
    * refactor splatfacto with parameterdict to simplify subclassing, defining new gaussian features
    
    * stray comment
    
    * fix exp_avg key error
    
    * backwards compatibility for checkpoint
    
    * backwards comp fix
    
    ---------
    
    Co-authored-by: Matias Turkulainen <30566358+maturk@users.noreply.github.com>

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -647,7 +647,6 @@ method_configs["splatfacto"] = TrainerConfig(
     steps_per_eval_all_images=1000,
     max_num_iterations=30000,
     mixed_precision=False,
-    gradient_accumulation_steps={"camera_opt": 100},
     pipeline=VanillaPipelineConfig(
         datamanager=FullImageDatamanagerConfig(
             dataparser=NerfstudioDataParserConfig(load_3D_points=True),
@@ -656,7 +655,7 @@ method_configs["splatfacto"] = TrainerConfig(
         model=SplatfactoModelConfig(),
     ),
     optimizers={
-        "xyz": {
+        "means": {
             "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(
                 lr_final=1.6e-6,
@@ -671,15 +670,15 @@ method_configs["splatfacto"] = TrainerConfig(
             "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
             "scheduler": None,
         },
-        "opacity": {
+        "opacities": {
             "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
             "scheduler": None,
         },
-        "scaling": {
+        "scales": {
             "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
             "scheduler": None,
         },
-        "rotation": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+        "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
         "camera_opt": {
             "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(lr_final=5e-5, max_steps=30000),
@@ -697,7 +696,6 @@ method_configs["splatfacto-big"] = TrainerConfig(
     steps_per_eval_all_images=1000,
     max_num_iterations=30000,
     mixed_precision=False,
-    gradient_accumulation_steps={"camera_opt": 100},
     pipeline=VanillaPipelineConfig(
         datamanager=FullImageDatamanagerConfig(
             dataparser=NerfstudioDataParserConfig(load_3D_points=True),
@@ -709,7 +707,7 @@ method_configs["splatfacto-big"] = TrainerConfig(
         ),
     ),
     optimizers={
-        "xyz": {
+        "means": {
             "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(
                 lr_final=1.6e-6,
@@ -724,15 +722,15 @@ method_configs["splatfacto-big"] = TrainerConfig(
             "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
             "scheduler": None,
         },
-        "opacity": {
+        "opacities": {
             "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
             "scheduler": None,
         },
-        "scaling": {
+        "scales": {
             "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
             "scheduler": None,
         },
-        "rotation": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+        "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
         "camera_opt": {
             "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(lr_final=5e-5, max_steps=30000),
